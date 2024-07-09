1c21942aaa31a27ade3fa3d73abb47898cd3cea1
VitaliiHoida <vitaliy.gojda@gmail.com>
2024-07-09 12:28:18 +0300

    edit back url

--- src/components/ItemComponent.vue
+++ src/components/ItemComponent.vue
@@ -2,71 +2,161 @@
   <div class="item_wrapper">
     <div class="pic_days">
       <div class="pic">
-        <img :src="require(`@/assets/img/${pic_name}`)" :alt="item_name"/>
+        <img :src="require(`@/assets/img/${pic_name}`)" :alt="item_name" />
       </div>
       <div class="days">
         <div>
           <p>{{ text_sm }}</p>
-          <CounterComponent :value="sm"
-                            @add="add('sm')"
-                            @remove="remove('sm')"
+          <CounterComponent
+            :value="sm"
+            @add="add('sm')"
+            @remove="remove('sm')"
           />
         </div>
 
         <div>
           <p>{{ text_lg }}</p>
-          <CounterComponent :value="lg"
-                            @add="add('lg')"
-                            @remove="remove('lg')"
+          <CounterComponent
+            :value="lg"
+            @add="add('lg')"
+            @remove="remove('lg')"
           />
         </div>
       </div>
     </div>
 
     <label class="custom_check" v-show="show_addit">
-      <input type="checkbox" class="check" v-model="addit">
+      <input type="checkbox" class="check" v-model="addit" />
       <span class="text">Додаткове обладнання:</span>
       <span class="checkmark"></span>
-      <svg width="70" height="30" viewBox="0 0 70 30" fill="none" xmlns="http://www.w3.org/2000/svg" class="add_pic">
+      <svg
+        width="70"
+        height="30"
+        viewBox="0 0 70 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="add_pic"
+      >
         <g clip-path="url(#clip0_318_2329)">
           <path
-              d="M7.5 20.4718V0.73043C7.5 0.569707 7.63031 0.439453 7.79098 0.439453H22.6777C22.8384 0.439453 22.9687 0.569766 22.9687 0.73043V29.2695C22.9687 29.4302 22.8384 29.5605 22.6777 29.5605H7.79098C7.63025 29.5605 7.5 29.4302 7.5 29.2695V20.4718Z"
-              stroke="" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            d="M7.5 20.4718V0.73043C7.5 0.569707 7.63031 0.439453 7.79098 0.439453H22.6777C22.8384 0.439453 22.9687 0.569766 22.9687 0.73043V29.2695C22.9687 29.4302 22.8384 29.5605 22.6777 29.5605H7.79098C7.63025 29.5605 7.5 29.4302 7.5 29.2695V20.4718Z"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
           <path
-              d="M15.2352 18.614C15.8073 18.614 16.2711 18.1502 16.2711 17.5781C16.2711 17.006 15.8073 16.5422 15.2352 16.5422C14.663 16.5422 14.1992 17.006 14.1992 17.5781C14.1992 18.1502 14.663 18.614 15.2352 18.614Z"
-              stroke="" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M11.8541 3H10.8145" stroke="" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M15.8939 3H14.8542" stroke="" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M19.9335 3H18.8938" stroke="" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M10.1367 23H19.8633" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M10.1367 25.1094H19.8633" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M10.1367 27.2188H19.8633" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
+            d="M15.2352 18.614C15.8073 18.614 16.2711 18.1502 16.2711 17.5781C16.2711 17.006 15.8073 16.5422 15.2352 16.5422C14.663 16.5422 14.1992 17.006 14.1992 17.5781C14.1992 18.1502 14.663 18.614 15.2352 18.614Z"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M11.8541 3H10.8145"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M15.8939 3H14.8542"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M19.9335 3H18.8938"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.1367 23H19.8633"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.1367 25.1094H19.8633"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.1367 27.2188H19.8633"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </g>
         <g clip-path="url(#clip1_318_2329)">
-          <path d="M63.3497 4.09155H43.0002" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M43.1219 4.09155H40.4395V24.9324H59.5246" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M62.6002 4.09155H69.5606V24.9324H58.8972" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M69.4995 21.2808H40.4995" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M54.0901 23.1125H55.9101" stroke="" stroke-miterlimit="10" stroke-linecap="round"
-                stroke-linejoin="round"/>
-          <path d="M56.8201 24.9326H53.1799V27.7406H56.8201V24.9326Z" stroke="" stroke-miterlimit="10"
-                stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M60.4601 27.7404H49.5398V29.5605H60.4601V27.7404Z" stroke="" stroke-miterlimit="10"
-                stroke-linecap="round" stroke-linejoin="round"/>
+          <path
+            d="M63.3497 4.09155H43.0002"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M43.1219 4.09155H40.4395V24.9324H59.5246"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M62.6002 4.09155H69.5606V24.9324H58.8972"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M69.4995 21.2808H40.4995"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M54.0901 23.1125H55.9101"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M56.8201 24.9326H53.1799V27.7406H56.8201V24.9326Z"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M60.4601 27.7404H49.5398V29.5605H60.4601V27.7404Z"
+            stroke=""
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </g>
         <defs>
           <clipPath id="clip0_318_2329">
-            <rect width="30" height="30" fill="white"/>
+            <rect width="30" height="30" fill="white" />
           </clipPath>
           <clipPath id="clip1_318_2329">
-            <rect width="30" height="30" fill="white" transform="translate(40)"/>
+            <rect
+              width="30"
+              height="30"
+              fill="white"
+              transform="translate(40)"
+            />
           </clipPath>
         </defs>
       </svg>
@@ -75,18 +165,23 @@
     <div class="summary">
       <span>Загальна вартість:</span><span class="value">{{ sum }}&nbsp;₴</span>
     </div>
-    <input class="promo" type="text" placeholder="введіть промо код" v-model="promo">
+    <input
+      class="promo"
+      type="text"
+      placeholder="введіть промо код"
+      v-model="promo"
+    />
     <button class="buy" type="button" @click="makeOrder()">Оплатити</button>
   </div>
 </template>
 
 <script>
 import CounterComponent from "@/components/CounterComponent.vue";
-import {useTelegram} from "@/helpers/useTelegram";
+import { useTelegram } from "@/helpers/useTelegram";
 import axios from "axios";
 
 export default {
-  name: 'ItemComponent',
+  name: "ItemComponent",
   components: {
     CounterComponent,
   },
@@ -118,32 +213,33 @@ export default {
     show_addit: {
       type: Boolean,
       required: false,
-    }
+    },
   },
   data() {
     return {
       sm: 0,
       lg: 0,
       addit: false,
-      promo: '',
-    }
+      promo: "",
+    };
   },
   computed: {
-    sum () {
+    sum() {
       if (this.sm > 0 || this.lg > 0) {
         if (this.addit) {
-          let res = (this.sm  * (this.price_sm + 100)) + (this.lg  * (this.price_lg + 1000))
-          this.promo === 'webnauts.pro' ? res = res * 0.5 : res;
-          return res
+          let res =
+            this.sm * (this.price_sm + 100) + this.lg * (this.price_lg + 1000);
+          this.promo === "webnauts.pro" ? (res = res * 0.5) : res;
+          return res;
         } else {
-          let res = (this.sm * this.price_sm) + (this.lg * this.price_lg)
-          this.promo === 'webnauts.pro' ? res = res * 0.5 : res;
-          return res
+          let res = this.sm * this.price_sm + this.lg * this.price_lg;
+          this.promo === "webnauts.pro" ? (res = res * 0.5) : res;
+          return res;
         }
       } else {
         return 0;
       }
-    }
+    },
   },
   methods: {
     add(variable) {
@@ -153,9 +249,9 @@ export default {
       this[variable]--;
     },
     makeOrder() {
-      const {tg, queryId, user, chatId} = useTelegram();
+      const { tg, queryId, user, chatId } = useTelegram();
       let order = {};
-      order.user_name = user.first_name + ' ' + user.last_name;
+      order.user_name = user.first_name + " " + user.last_name;
       order.sum_to_pay = this.sum;
       order.name = this.item_name;
       order.text_sm = this.text_sm;
@@ -165,44 +261,44 @@ export default {
       order.addit = this.addit;
 
       tg.MainButton.setParams({
-        text: 'Сплатити ' + order.sum_to_pay + ' грн.',
-        color: '#217C2F',
+        text: "Сплатити " + order.sum_to_pay + " грн.",
+        color: "#217C2F",
       });
 
       tg.MainButton.show();
 
       const mainButtonHandler = function () {
-
         const data = {
           order,
           queryId,
-          chatId
+          chatId,
         };
 
-        axios.post('https://api.wsbot.staj.fun/web-data', data, {headers: {'Content-Type': 'application/json'}}).then(
-            (response) => {
-              tg.openInvoice(response.data, function (status) {
-                if (status == 'paid') {
-                  tg.close();
-                } else if (status == 'failed') {
-                  tg.HapticFeedback.notificationOccurred('error');
-                } else {
-                  tg.HapticFeedback.notificationOccurred('warning');
-                }
-              });
-            }
-        );
-
+        axios
+          .post("https://wsbot.webnauts.space/web-data", data, {
+            headers: { "Content-Type": "application/json" },
+          })
+          .then((response) => {
+            tg.openInvoice(response.data, function (status) {
+              if (status == "paid") {
+                tg.close();
+              } else if (status == "failed") {
+                tg.HapticFeedback.notificationOccurred("error");
+              } else {
+                tg.HapticFeedback.notificationOccurred("warning");
+              }
+            });
+          });
 
         tg.MainButton.hide();
 
-        tg.offEvent('mainButtonClicked', mainButtonHandler);
+        tg.offEvent("mainButtonClicked", mainButtonHandler);
       };
 
-      tg.onEvent('mainButtonClicked', mainButtonHandler);
+      tg.onEvent("mainButtonClicked", mainButtonHandler);
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -294,7 +390,7 @@ export default {
 }
 
 .custom_check input:checked ~ .checkmark {
-  background-color: #E00822;
+  background-color: #e00822;
 }
 
 .checkmark:after {
@@ -346,7 +442,7 @@ export default {
 
 .promo {
   box-sizing: border-box;
-  border: 1px solid #E00822 !important;
+  border: 1px solid #e00822 !important;
   font-weight: 700;
   font-size: 14px;
   line-height: 14px;
@@ -359,14 +455,14 @@ export default {
 }
 
 input.promo:focus-visible {
-  border: 1px solid #E00822 !important;
+  border: 1px solid #e00822 !important;
   outline: none;
 }
 
 .buy {
   margin: 10px 0 30px 0;
-  background: #E00822;
-  border: 1px solid #E00822;
+  background: #e00822;
+  border: 1px solid #e00822;
   border-radius: 50px;
   font-weight: 700;
   font-size: 16px;
